25b94b923f597f0870c5ad1e126f5e81dbd24b68
vd89 <dixitvora@gmail.com>
2023-06-26 16:42:24 +0530

    main: added all the controller and reoutes for the contect

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,12 +2,55 @@ import debug from "debug";
 import contents from '../helper/contentsHelper.js';
 const logger = debug('app:contentController -> ')
 
-export const getAllContents = async (req,res) => {
-  try {
-    const newContents = contents()
-    return res.ok({message:"SUCCESS", data: newContents})
-  } catch (err) {
-    logger(err.message);
-    next(err);
-  }
+export const getAllContents = async (req, res, next) => {
+ try {
+  const newContents = contents;
+  return res.ok({ message: 'SUCCESS', data: newContents });
+ } catch (err) {
+  logger(err.message);
+  next(err);
  }
+};
+
+export const getContentsById = async (req, res, next) => {
+ try {
+  const { id } = req.params;
+  const newContents = contents.filter((item) => item.id == id);
+  return res.ok({ message: 'SUCCESS', data: newContents });
+ } catch (err) {
+  logger(err.message);
+  next(err);
+ }
+};
+
+export const createContents = async (req, res, next) => {
+ try {
+  const { name, email, phone } = req.body;
+  const { id } = req.params;
+  if (id) {
+   const item = { id, name, email, phone };
+   contents[contents.findIndex((ele) => ele.id == id)] = item;
+
+   return res.ok({ message: 'SUCCESS', data: item });
+  }
+  const newId = contents.length + 1;
+  const newContact = { name, email, phone, id: newId };
+  contents.push(newContact);
+
+  return res.ok({ message: 'SUCCESS', data: newContact });
+ } catch (err) {
+  logger(err.message);
+  next(err);
+ }
+};
+
+export const deleteContent = async (req, res, next) => {
+ try {
+  const { id } = req.params;
+  const contacts = contents.filter((item) => item.id != id);
+  return res.ok({ message: 'SUCCESS', data: contacts });
+ } catch (err) {
+  logger(err.message);
+  next(err);
+ }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 
-import { testAuth } from '../helper/extraHelper.js';
-import { getAllContents } from '../controllers/index.js';
+import { createContents, deleteContent, getAllContents, getContentsById } from '../controllers/index.js';
 
 
 const apiRoutes = new Router();
 
 apiRoutes.get('/contacts', getAllContents) //  api route -- /api/contacts --
-apiRoutes.get('/contacts/:id', testAuth) // added the controller for the get individual content
-apiRoutes.post('/contacts',testAuth) // create a content with the route
-apiRoutes.put('/contacts/:id', testAuth) // updated the content with the data
-apiRoutes.delete('/contacts/:id', testAuth) // delete the desired content
+apiRoutes.get('/contacts/:id', getContentsById) // added the controller for the get individual content
+apiRoutes.post('/contacts',createContents) // create a content with the route
+apiRoutes.put('/contacts/:id', createContents) // updated the content with the data
+apiRoutes.delete('/contacts/:id', deleteContent) // delete the desired content
 
 export default apiRoutes;
